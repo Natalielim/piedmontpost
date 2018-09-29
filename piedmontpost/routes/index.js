f1c3f3bca48d0d1d1172921f13d96a26d2253295
natalielim <natalielim@gmail.com>
2018-09-29 16:49:38 -0700

    Added routes and footer

--- piedmontpost/routes/index.js
+++ piedmontpost/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Piedmont Post' });
+});
+
+// GET underwriters
+router.get('/underwriters', (req, res, next) => {
+  res.render('underwriters');
+});
+
+// GET subscribe
+router.get('/subscribe', (req, res, next) => {
+  res.render('subscribe');
+});
+
+// GET contact
+router.get('/contact', (req, res, next) => {
+  res.render('contact');
 });
 
 module.exports = router;
